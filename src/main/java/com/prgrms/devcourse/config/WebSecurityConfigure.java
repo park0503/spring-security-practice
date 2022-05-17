626b28a15a32385c7cbe0d41604c7d6a5b3d8792
park0503 <hooo0503@gmail.com>
2022-05-17 21:43:23 +0900

    1일차 과제 솔루션 적용

--- src/main/java/com/prgrms/devcourse/config/WebSecurityConfigure.java
+++ src/main/java/com/prgrms/devcourse/config/WebSecurityConfigure.java
@@ -6,42 +6,32 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
-import org.springframework.security.crypto.password.*;
-import org.springframework.security.crypto.scrypt.SCryptPasswordEncoder;
-
-import java.util.HashMap;
-import java.util.Map;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfigure extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        String idForEncode = "noop";
-        Map<String, PasswordEncoder> encoders = new HashMap<>();
-        encoders.put("bcrypt", new BCryptPasswordEncoder());
-        encoders.put(idForEncode, org.springframework.security.crypto.password.NoOpPasswordEncoder.getInstance());
-        encoders.put("pbkdf2", new Pbkdf2PasswordEncoder());
-        encoders.put("scrypt", new SCryptPasswordEncoder());
-        encoders.put("sha256", new StandardPasswordEncoder());
-        PasswordEncoder passwordEncoder = new DelegatingPasswordEncoder(idForEncode, encoders);
+        PasswordEncoder passwordEncoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
 
         auth
                 .inMemoryAuthentication()
                 .withUser("user")
-                .password(passwordEncoder.encode("user123"))
+                .password("{noop}user123")
                 .roles("USER")
                 .and()
+
                 .withUser("admin")
-                .password(passwordEncoder.encode("admin123"))
+                .password("{noop}admin123")
                 .roles("ADMIN");
     }
 
     @Override
     //필터 체인 관련 전역 설정을 처리할 수 있는 API 제공)
-    public void configure(WebSecurity web) throws Exception {
+    public void configure(WebSecurity web) {
         web.ignoring().antMatchers("/assets/**");
         // 지정된 ant Path에 매칭되는 요청에 대해서는 시큐리티 필터체인을 태우지 않겠다.
         //불필요한 자원 소모 방지
@@ -54,14 +44,20 @@ public class WebSecurityConfigure extends WebSecurityConfigurerAdapter {
                 .antMatchers("/me").hasAnyRole("USER", "ADMIN")
                 .anyRequest().permitAll()
                 .and()
+
                 .formLogin()//스프링 시큐리티가 자동으로 로그인 페이지를 생성해 주는 것을 생성
                 .defaultSuccessUrl("/me") //성공 시 리다이렉트 페이지
                 .permitAll() //로그인 페이지는 익명 페이지
                 .and()
+
                 .logout()
-                .logoutUrl("/logout")
+                // 아래 매쳐는 default 값과 똑같지만 없으면 한번 더 물어봄
+                .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
                 .logoutSuccessUrl("/")
+//                .invalidateHttpSession(true)// default라 없어도 됨
+//                .clearAuthentication(true)// default라 없어도 됨
                 .and()
+
                 .rememberMe()
                 .rememberMeParameter("remember-me")
                 .tokenValiditySeconds(300)
